f679c5d6840dde7d66e99b7235d18dfa4f279d9e
Sialsdad <124737490+Sialsdad@users.noreply.github.com>
2023-02-08 10:20:43 -0800

    Add files via upload

---
+++ RandomSound/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace RandomSound
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "gorillatag.randomsound";
+        public const string Name = "RandomSound";
+        public const string Version = "1.0.0";
+    }
+}
